bb2508e52ea2cf7c6661bb313fc9d7671dbd7c2f
idsc3101 <idsc3101@gmail.com>
2013-09-23 15:20:08 -0500

    Continued working on Work Day 3 in class today. Focused on If-Then commands.

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,20 +4,29 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
 
-        Dim num1, num2, largerNum As Double
+        Dim num1, num2 As Double
+
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+            MessageBox.Show("error: please type a number")
+            Return
+        End If
 
         num1 = CDbl(txtFirstNum.Text)
         num2 = CDbl(txtSecondNum.Text)
 
-        If (num1 > num2) Then
-            largerNum = num1
-        ElseIf (num1 < num2) Then
-            largerNum = num2
-            txtResult.Text = "The larger number is " & num2
-
+        If num1 < 0 Or num2 < 0 Then
+            MessageBox.Show("error: please type a number")
+            Return
         End If
 
-        txtResult.Text = "THe larger number is " & largerNum
+        If (num1 > num2) Then
+            txtResult.Text = "Larger number is" & num1
+        ElseIf (num1 < num2) Then
+            txtResult.Text = "Larger number is " & num2
+        Else
+            txtResult.Text = "The two are equal."
+        End If
+        
 
     End Sub
 End Class
